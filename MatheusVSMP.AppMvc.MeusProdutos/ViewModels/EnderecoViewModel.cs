57ed321b207fce2d6756da5a84ca0efe5c5f769b
MatheusVSMPimentel <matheusvsm.pimentel@hotmail.com>
2024-06-27 19:06:37 -0300

    feat: viewmodels create, identy models config, add auto mapper tecnology

---
+++ MatheusVSMP.AppMvc.MeusProdutos/ViewModels/EnderecoViewModel.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.ComponentModel.DataAnnotations;
+using System.Web.Mvc;
+using static MatheusVSMP.Business.Core.Constants.MensagensConstantes;
+
+namespace MatheusVSMP.AppMvc.MeusProdutos.ViewModels
+{
+    public class EnderecoViewModel
+    {
+        public EnderecoViewModel()
+        {
+            Id = Guid.NewGuid();
+        }
+
+        [Key]
+        public Guid Id { get; set; }
+        [Required(ErrorMessage = RequiredDataAnnotations)]
+        [StringLength(200, MinimumLength = 2, ErrorMessage = StringLegthDataAnnotations)]
+        public string Logradouro { get; set; }
+        [Required(ErrorMessage = RequiredDataAnnotations)]
+        [StringLength(50, MinimumLength = 1, ErrorMessage = StringLegthDataAnnotations)]
+        public string Numero { get; set; }
+        public string Complemento { get; set; }
+        [Required(ErrorMessage = RequiredDataAnnotations)]
+        [StringLength(8, ErrorMessage = RequiredStringLegthDataAnnotations)]
+        public string Cep { get; set; }
+        [Required(ErrorMessage = RequiredDataAnnotations)]
+        [StringLength(100, MinimumLength = 2, ErrorMessage = StringLegthDataAnnotations)]
+        public string Bairro { get; set; }
+        [Required(ErrorMessage = RequiredDataAnnotations)]
+        [StringLength(100, MinimumLength = 2, ErrorMessage = StringLegthDataAnnotations)]
+        public string Cidade { get; set; }
+        [Required(ErrorMessage = RequiredDataAnnotations)]
+        [StringLength(50, MinimumLength = 2, ErrorMessage = StringLegthDataAnnotations)]
+        public string Estado { get; set; }
+        [HiddenInput]
+        public Guid FornecedorId { get; set; }
+    }
+}
---
+++ MatheusVSMP.AppMvc.MeusProdutos/ViewModels/FornecedorViewModel.cs
@@ -0,0 +1,31 @@
+﻿using static MatheusVSMP.Business.Core.Constants.MensagensConstantes;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.ComponentModel.DataAnnotations;
+
+namespace MatheusVSMP.AppMvc.MeusProdutos.ViewModels
+{
+    public class FornecedorViewModel
+    {
+        public FornecedorViewModel()
+        {
+            Id = Guid.NewGuid();
+        }
+        [Key]
+        public Guid Id { get; set; }
+        [Required(ErrorMessage = RequiredDataAnnotations)]
+        [StringLength(100, MinimumLength = 2, ErrorMessage = StringLegthDataAnnotations)]
+        public string Nome { get; set; }
+        [Required(ErrorMessage = RequiredDataAnnotations)]
+        [StringLength(14, MinimumLength = 11, ErrorMessage = StringLegthDataAnnotations)]
+        public string Documento { get; set; }
+        [DisplayName("Tipo")]
+        public int TipoFornecedor { get; set; }
+        [DisplayName("Ativo?")]
+        public bool Ativo { get; set; }
+
+        public EnderecoViewModel Endereco { get; set; }
+        public IEnumerable<ProdutoViewModel> Produtos { get; set; }
+    }
+}
